d24f18dc902b1b25eb16fbedb932717a38478835
Shaharyar <Shaharyar321321@gmail.com>
2024-02-16 15:32:53 +0500

    Add form for new website (no server side impl yet)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Toaster } from "sonner";
 import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,6 +28,7 @@ export default async function RootLayout({
         >
           <main>{children}</main>
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import { Button } from "./ui/button";
 import { signIn } from "next-auth/react";
+import { Dialog, DialogTrigger, DialogContent, DialogClose } from "./ui/dialog";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,8 +13,27 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import { UserDialogBox } from "./user-dialog-box";
+import { Plus } from "lucide-react";
+import { Input } from "./ui/input";
+import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+type FormData = {
+  protocol?: string;
+  url?: string;
+  tags?: string[];
+};
 
 export const Navbar = ({
   username,
@@ -22,6 +42,14 @@ export const Navbar = ({
   username: Nullable<string>;
   avatar: Nullable<string>;
 }) => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [protocol, setProtocol] = useState<string>();
+  const [url, setUrl] = useState<string>();
+  const [formData, setFormData] = useState<FormData>();
+  useEffect(() => {
+    setFormData({ protocol, url, tags });
+  }, [tags, protocol, url]);
+
   return (
     <nav className="relative w-full">
       <div className="min-h-12 flex justify-between items-center py-2 mx-4 lg:mx-12">
@@ -31,26 +59,99 @@ export const Navbar = ({
         >
           Website Ratings
         </span>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Top Websites</NavigationMenuTrigger>
-              <NavigationMenuContent className="">
-                <>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Developers Websites</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="flex gap-2">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Top Websites</NavigationMenuTrigger>
+                <NavigationMenuContent className="">
+                  <>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                  </>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  Developers Websites
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                  </>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <Dialog
+            onOpenChange={() => {
+              setTimeout(() => {
+                setTags([]);
+                setProtocol("");
+                setUrl("");
+              }, 100);
+            }}
+          >
+            <DialogTrigger asChild>
+              <Button variant={"ghost"}>
+                <Plus /> Add New
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <h2>Add New Website</h2>
+              <form
+                className="flex gap-4 flex-col"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  toast(<pre>{JSON.stringify(formData, null, 2)}</pre>);
+                }}
+              >
+                <Select
+                  onValueChange={(e) => {
+                    setProtocol(e);
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Protocol" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="http">HTTP</SelectItem>
+                    <SelectItem value="https">HTTPS</SelectItem>
+                  </SelectContent>
+                </Select>
+                <Input
+                  onChange={(e) => {
+                    setUrl(e.target.value);
+                  }}
+                  type="text"
+                  placeholder="URL"
+                />
+                <div className="flex gap-2">
+                  <Input
+                    onChange={(e) => {
+                      setTags(e.target.value.split(","));
+                    }}
+                    type="text"
+                    placeholder="Tags"
+                  />
+                  <span className="flex text-white/40 justify-center items-center">
+                    {tags.length}/6
+                  </span>
+                </div>
+                <div className="flex gap-2">
+                  {tags.map((tag, idx) => {
+                    if (tags[0] !== "")
+                      return <Badge key={tag + idx}>{tag}</Badge>;
+                  })}
+                </div>
+                <DialogClose>
+                  <Button className="w-full" type="submit" value="Submit">
+                    Submit
+                  </Button>
+                </DialogClose>
+              </form>
+            </DialogContent>
+          </Dialog>
+        </div>
         {username ? (
           <UserDialogBox name={username} avatar={avatar} />
         ) : (
